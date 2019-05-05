a9a63240d6fadedde02319ae2e6de8dea627bd44
gcem <34423323+gcem@users.noreply.github.com>
2019-05-05 12:31:51 +0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,94 @@
+// 
+// An OpenGL program template based on Samanta Guha's circle.cpp
+//
+//
+//
+
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+
+#ifndef __APPLE__
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <GL/glext.h>
+#pragma comment(lib, "glew32.lib")
+#else
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <OpenGL/glext.h>
+#endif
+
+
+#define PI 3.14159265358979324
+
+using namespace std;
+
+// Global variables
+static float X = 50;
+static float Y = 50;
+
+void drawScene(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glColor3f(0.0, 0.0, 0.0);
+
+  glBegin(GL_TRIANGLE_FAN);
+  glVertex3f(10.0, 10.0, 0.0);
+  glVertex3f(10.0, 30.0, 0.0);
+  glVertex3f(20.0, 80.0, 0.0);
+  glVertex3f(80.0, 20.0, 0.0);
+
+  glEnd();
+
+  glFlush();
+}
+
+void setup(void)
+{
+  glClearColor(1.0, 1.0, 1.0, 0.0);
+}
+
+void resize(int width, int height)
+{
+  glViewport(0, 0, width, height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0.0, 100.0, 1.0, 100.0, -1.0, 1.0);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+void keyInput(unsigned char key, int x, int y)
+{
+  switch (key)
+    {
+    case 27:
+      exit(0);
+      break;
+    }
+}
+
+int main(int argc, char** argv)
+{
+  glutInit(&argc, argv);
+
+  glutInitContextVersion(4, 3);
+  glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(200, 200);
+  glutCreateWindow("window");
+  glutDisplayFunc(drawScene);
+  glutReshapeFunc(resize);
+  glutKeyboardFunc(keyInput);
+
+  glewExperimental = GL_TRUE;
+  glewInit();
+
+  setup();
+
+  glutMainLoop();
+}
